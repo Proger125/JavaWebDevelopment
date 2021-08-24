f2124dd106f7c6b6207725043eb5ed357fdb2929
Proger_125 <alexdyachenka@gmail.com>
2021-08-24 20:37:25 +0300

    Adding default command

--- WebProject/src/main/java/edu/epam/webproject/controller/command/impl/DefaultCommand.java
+++ WebProject/src/main/java/edu/epam/webproject/controller/command/impl/DefaultCommand.java
@@ -5,6 +5,7 @@ import edu.epam.webproject.controller.command.PagePath;
 import edu.epam.webproject.controller.command.Router;
 import jakarta.servlet.http.HttpServletRequest;
 
+
 public class DefaultCommand implements Command {
     @Override
     public Router execute(HttpServletRequest req) {
